08a105f1b57afbc377cf66157730b49e39f49fdf
Evgeniy Gichev <35485232+gicheve@users.noreply.github.com>
2018-08-11 00:09:32 +0300

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+--[[
+wifi.setmode(wifi.STATION)
+wifi.sta.config("EvgNik","mamaesuper")
+--wifi.sta.config("Vidima_Guests","ISIGuestAP1")
+--wifi.sta.setip({ip="192.168.0.127",netmask="255.255.255.0",gateway="192.168.0.1"})
+wifi.sta.connect()
+wifi.sta.sleeptype(wifi.NONE_SLEEP)
+]]
 valvepos = 1
 templow = 1
 temphigh = 1
@@ -11,26 +19,22 @@ temprtbl = {0,0,0,0}
 
 enduser_setup.start(
   function()
---   tmr.alarm(1, 1000, 1, function()
---     if wifi.sta.getip()== nil then
---          print("IP unavaiable, Waiting...")
---     else
---	  wifi.sta.sleeptype(wifi.NONE_SLEEP)
---      tmr.stop(1)
---	  enduser_setup.stop()
-	  print("Connected to wifi as:" .. wifi.sta.getip())
-	  print("ESP8266 mode is: " .. wifi.getmode())
-	  print("The module MAC address is: " .. wifi.ap.getmac())
---	  
-	 -- Start the WEB server
-	 tmr.alarm(1, 6000, 0, function()
-		 enduser_setup.stop()
-		 sntp.sync()
-		 print("Ready: "..rtctime.get())
-		 dofile("config.lua")
-		 dofile("www.lua")
-	  
---     end
+   tmr.alarm(1, 1000, 1, function()
+     if wifi.sta.getip()== nil then
+        print("IP unavaiable, Waiting...")
+     else
+        sntp.sync()
+        wifi.sta.sleeptype(wifi.NONE_SLEEP)
+        tmr.stop(1)
+        enduser_setup.stop()
+        print("Connected to wifi as:" .. wifi.sta.getip())
+        print("ESP8266 mode is: " .. wifi.getmode())
+        print("The module MAC address is: " .. wifi.ap.getmac())	  
+  -- Start the WEB server
+       print("Ready: "..rtctime.get())
+       dofile("config.lua")
+       dofile("www.lua")
+    end
     end)
           end,
       function(err, str)
@@ -40,12 +44,12 @@ enduser_setup.start(
       end,
     
   -- Lua print function can serve as the debug callback
-  tmr.alarm(3, 60000, 0, function()
-    if wifi.sta.getip() == nil  then node.restart() end
-  end)
- 
-);
-
+    tmr.alarm(3, 60000, 0, function()
+      if wifi.sta.getip()== nil then
+         node.restart()
+      end
+    end)
+  );
 
 tmr.alarm(4, 3600000, 1, function()
     sntp.sync()
